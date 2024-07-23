81fe60c6bc0fad0e36c7101551c4ebe6000f67f9
MorgynKass <morgynkass@gmail.com>
2024-07-23 13:20:28 -0600

    finished visual feedback on buttons

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,46 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton({ children }) {
+  function buttonHandler() {
+    console.log("Pressed");
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.buttonPressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={buttonHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.button}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    margin: 4,
+    borderRadius: 28,
+    overflow: "hidden",
+    elevation: 2,
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  button: {
+    color: "white",
+    textAlign: "center",
+  },
+  buttonPressed: {
+    opacity: 0.75,
+  },
+});
 
 export default PrimaryButton;
